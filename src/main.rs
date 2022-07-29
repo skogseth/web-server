4422dcdceeeef1b386218758c9f0428659be90ae
skogseth <herman.skogseth@me.com>
2022-07-29 17:55:31 +0200

    Included dependencies and created a basic test setup using async and await

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let mut handles = Vec::new();
+
+    for i in 0..3 {
+        let handle = tokio::spawn(async move {
+            task(i).await;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.expect("Error retrieving task.");
+    }
+}
+
+async fn task(i: u64) {
+    println!("Hello from task {i}, beginning computation...");
+    tokio::time::sleep(Duration::from_secs(i)).await;
+    println!("Task {i} signing off.");
 }
